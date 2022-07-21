56dfb24c4914124d6d720f6f0de7982748df0274
denisjovic <redzistrejsn@pm.me>
2022-07-21 19:12:00 +0200

    Add readme

---
+++ README.md
@@ -0,0 +1,25 @@
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/denisjovic/maps.git
+```
+
+Go to the project directory
+
+```bash
+  cd my-project
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  parcel index.html
+```
--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -15,9 +15,16 @@ export class CustomMap {
   }
 
   addMarker(mappable: MapProps): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: { lat: mappable.location.lat, lng: mappable.location.lng },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: `<h1>'Hi there</h1>`,
+      });
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
